8bbeb8e1c5065510aa8ce18334d6ca09fbe4fc01
med lehbib <devexpress2013@gmail.com>
2023-06-16 14:48:28 +0000

    Commit de 16 juin 2023

--- librairie/library_member/__manifest__.py
+++ librairie/library_member/__manifest__.py
@@ -1,8 +1,16 @@
 {
-"name": "Library Members",
-"license": "AGPL-3",
-"description": "Manage members borrowing books.",
-"author": "Daniel Reis",
-"depends": ["library_app"],
-"application": False,
+    "name": "Library Members",
+    "license": "AGPL-3",
+    "description": "Manage members borrowing books.",
+    "author": "Daniel Reis",
+    "depends": ["librairie_app"],
+    "application": False,
+     "data": [
+        #"security/library_security.xml",
+        #"security/ir.model.access.csv",
+        "views/book_view.xml",
+        #"views/member_view.xml",
+        #"views/library_menu.xml",
+        #"views/book_list_template.xml",
+    ],
 }
